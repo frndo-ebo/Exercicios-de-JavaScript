7f31e2676468e71a29c533fba4ce776e64c6b861
frndo-ebo <fernando.bessa0301@gmail.com>
2026-03-13 19:18:13 -0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,7 @@
+//Cálculo de média de aluno
+
+let m1 = (4*2)
+let m2 = (5*3)
+let m3 = (3*5)
+let total = (m1 + m2 + m3) / 10
+console.log(`A média de um aluno é ${total}, suas notas foram de ${m1} (peso 2), ${m2} (peso 3), ${m3} (peso 5)`)
